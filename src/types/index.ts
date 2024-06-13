43a07decc2982bcc0cfe9a95f572a382ce339f94
Alex <rossetka@mail.ru>
2024-06-13 20:34:02 +0300

    making edits

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,39 +1,52 @@
 // ТИПЫ ДАННЫХ
 
-//продукта
+//для продукта
 export interface Product {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number;
-  }
-  
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}
+
 export interface ProductList {
-    total: number;
-    items: Product[];
-  }
-  
+  total: number;
+  items: Product[];
+}
+
 export interface ProductItem {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number;
-    error: string;
-  }
-  
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+  error: string;
+}
+
 //для заказа
 export interface Order {
-    id: string;
-    total: number;
-    error: string;
-  }
-  
+  id: string;
+  total: number;
+  error: string;
+}
+
 //для формы контактов
-export interface contacts {
-    email: string;
-    phone: number;
-  }
+export interface Сontact {
+  email: string;
+  phone: number;
+}
+
+//для формы оплаты
+export interface PaymentMethod {
+  type: 'online' | 'cash';
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
