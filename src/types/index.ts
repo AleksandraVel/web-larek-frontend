9b50b1031445e330a7d361f5d734a08f49c19fb0
Alex <rossetka@mail.ru>
2024-07-10 21:34:44 +0300

    making changes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,87 +1,55 @@
-import { Product } from '../components/AppData';
+// ТИПЫ ДАННЫХ
 
-export type CategoryType =
-  | 'другое'
-  | 'софт-скил'
-  | 'дополнительное'
-  | 'кнопка'
-  | 'хард-скил';
-
-export type CategoryMapping = {
-  [Key in CategoryType]: string;
-};
-
-export type FormErrors = Partial<Record<keyof IOrderForm, string>>;
-
-export interface ApiResponse {
-  items: IProduct[];
-}
-
-// Интерфейс, описывающий поля товара в магазине
-export interface IProduct {
+//для продукта
+export interface Product {
   id: string;
   description: string;
   image: string;
   title: string;
-  category: CategoryType;
-  price: number | null;
-  selected: boolean;
+  category: string;
+  price: number;
 }
 
-/*
-  * Интерфейс описывающий внутренне состояние приложения
-    Используется для хранения карточек, корзины, заказа пользователя, ошибок
-    при вообще в формах
-    Так же имеет методы для работы с карточками и корзиной
-  * */
-export interface IAppState {
-  // Корзина с товарами
-  basket: Product[];
-  // Массив карточек товара
-  store: Product[];
-  // Информация о заказе при покупке товара
-  order: IOrder;
-  // Ошибки при заполнении форм
-  formErrors: FormErrors;
-  // Метод для добавления товара в корзину
-  addToBasket(value: Product): void;
-  // Метод для удаления товара из корзины
-  deleteFromBasket(id: string): void;
-  // Метод для полной очистки корзины
-  clearBasket(): void;
-  // Метод для получения количества товаров в корзине
-  getBasketAmount(): number;
-  // Метод для получения суммы цены всех товаров в корзине
-  getTotalBasketPrice(): number;
-  // Метод для добавления ID товаров в корзине в поле items для order
-  setItems(): void;
-  // Метод для заполнения полей email, phone, address, payment в order
-  setOrderField(field: keyof IOrderForm, value: string): void;
-  // Валидация форм для окошка "контакты"
-  validateContacts(): boolean;
-  // Валидация форм для окошка "заказ"
-  validateOrder(): boolean;
-  // Очистить order после покупки товаров
-  refreshOrder(): boolean;
-  // Метод для превращения данных, полученых с сервера в тип данных приложения
-  setStore(items: IProduct[]): void;
-  // Метод для обновления поля selected во всех товарах после совершения покупки
-  resetSelected(): void;
-}
-
-// Интерфейс, описывающий поля заказа товара
-export interface IOrder {
-  items: string[];
-  payment: string;
+export interface ProductList {
   total: number;
-  address: string;
+  items: Product[];
+}
+
+
+export interface CardData {
+  product: Product;
+  onAddToBasket: (product: Product) => void;
+}
+
+//для формы контактов
+export interface IСontact {
   email: string;
   phone: string;
 }
 
-export interface IOrderForm {
-  payment: string;
-  address: string;
-  email: string;
-  phone: string;
+//для заказа
+export interface IOrder {
+  paymentMethod: PaymentMethod;
+  address: Address;
 }
+
+//для формы оплаты
+export interface PaymentMethod {
+  type: 'online' | 'cash';
+}
+
+export interface Address {
+  [key: string]: string | number;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
+
+// Тип для элемента корзины
+export type BasketItem = {
+  quantity: number;
+  id: string;
+  product: Product; 
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,8 @@
-import { CategoryMapping } from "../types";
+//API_URL - используется для запросов данных о товарах и отправки заказа
+//CDN_URL - используется для формирования адреса картинки в товаре.
 
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-export const settings = {};
-
-export const categoryMapping: CategoryMapping = {
-  другое: "card__category_other",
-  "софт-скил": "card__category_soft",
-  дополнительное: "card__category_additional",
-  кнопка: "card__category_button",
-  "хард-скил": "card__category_hard",
+export const settings = {
 };
